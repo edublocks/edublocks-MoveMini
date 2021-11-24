3cac2257c736a3d15fe9388fdacaea019ae7e038
Joshua Lowe <joshualowe1002@gmail.com>
2021-11-24 20:19:25 +0000

    Create definitions.ts

---
+++ definitions.ts
@@ -0,0 +1,58 @@
+Blockly.Blocks['import_movemini'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("from movemini import *");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour("#02AD4D");
+      this.setTooltip("Imports movemini library.");
+      this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['movemini_init'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable("robot"), "robot")
+        .appendField(" = MoveMini()");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#02AD4D");
+  this.setTooltip("Initialise movemini");
+  this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['movemini_direction'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable("robot"), "robot")
+        .appendField(".")
+        .appendField(new Blockly.FieldDropdown([["forward","forward"], ["backward","backward"], ["left","left"], ["right","right"]]), "directions")
+        .appendField("(");
+    this.appendValueInput("NAME")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(")");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#02AD4D");
+  this.setTooltip("Drive the movemini");
+  this.setHelpUrl("");
+  }
+};
+
+
+Blockly.Blocks['movemini_stop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable("robot"), "robot")
+        .appendField(".stop()");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour("#02AD4D");
+  this.setTooltip("Stop the robot");
+  this.setHelpUrl("");
+  }
+};
